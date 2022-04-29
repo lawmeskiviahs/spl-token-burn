2cf2f46c7a7d20430f69050112bff38552fdc325
lawmeskiviahs <shaiviksemwal@gmail.com>
2022-04-29 18:33:38 +0530

    client refined

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,32 +1,16 @@
-/**
- * Hello world
- */
-
 import {
   establishConnection,
   establishPayer,
-  checkProgram,
   sayHello,
-  reportGreetings,
 } from './hello_world';
 
 async function main() {
-  console.log("Let's say hello to a Solana account...");
 
-  // Establish connection to the cluster
+  console.log("Client started executing");
+
   await establishConnection();
-
-  // Determine who pays for the fees
   await establishPayer();
-
-  // Check if the program has been deployed
-  // await checkProgram();
-
-  // Say hello to an account
   await sayHello();
-
-  // Find out how many times that account has been greeted
-  // await reportGreetings();
 
   console.log('Success');
 }
