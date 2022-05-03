87a2d86f7ab457d60ed53e5637d7501fb9148d70
lawmeskiviahs <shaiviksemwal@gmail.com>
2022-05-03 11:11:53 +0530

    ATA find p address

--- src/client/hello_world.ts
+++ src/client/hello_world.ts
@@ -8,11 +8,12 @@ import {
 } from '@solana/web3.js';
 import { Metadata } from "@metaplex-foundation/mpl-token-metadata";
 import {getPayer, getRpcUrl} from './utils';
+import { TOKEN_PROGRAM_ID } from '@solana/spl-token';
+import * as bs58 from "bs58";
 
 let connection: Connection;
 let payer: Keypair;
 let programId: PublicKey;
-let tokenprogram : PublicKey;
 
 export async function establishConnection(): Promise<void> {
   const rpcUrl = await getRpcUrl();
@@ -27,32 +28,53 @@ export async function establishPayer(): Promise<void> {
 
 export async function sayHello(): Promise<void> {
   // console.log('Saying hello to', greetedPubkey.toBase58());
-  tokenprogram = new PublicKey('TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA');
-  const mint = new PublicKey('FVjm9NswmXbnPinGnU2Zooj4CrZKsYFHqrxaCvV8dUpH');
-  const ata = new PublicKey('FXvXaFbaRmo4NrFbiTsax4HVQrTnHEovZRddwiK2dXAL');
+
+  const mint = new PublicKey('J6PXH6vJZhS8SNzVqathiRCLPwmsetAYQHSqwgadofxJ');
+
+  const SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID: PublicKey = new PublicKey(
+    'ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL',
+  );
+
+  const associatedtokenaccount = (await PublicKey.findProgramAddress(
+    [
+        payer.publicKey.toBuffer(),
+        TOKEN_PROGRAM_ID.toBuffer(),
+        mint.toBuffer(),
+    ],
+    SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID
+  ))[0];
+
   // console.log('reached instruction');
   programId = new PublicKey('ByPy64kKUyfLHkAQQRPxEpC5ocuWXjXBeMcpbm8A6YgR');
   
   let mintPubkeya = new PublicKey("AUa5RUdky6whb3AJ351yd9vpzRm5tpvE6W4CgUnFJ72p");
   let tokenmetaPubkeya = await Metadata.getPDA(mintPubkeya);
   let resultt = await Metadata.load(connection, tokenmetaPubkeya);
+
   let mintPubkeyb = new PublicKey("BSqDDYwFKmoJmjvjtNdhgcWT4J65Xd9tiE1buc6cL3gz");
   let tokenmetaPubkeyb = await Metadata.getPDA(mintPubkeyb);
-  let resulttt = await Metadata.load(connection, tokenmetaPubkeyb);
-  console.log(resultt.data.data.uri);
-  console.log(resulttt.data.data.uri);
+  // let resulttt = await Metadata.load(connection, tokenmetaPubkeyb);
+
+  console.log(resultt);
+  // console.log(resulttt.data.data.uri);
+
+  const signer =Keypair.fromSecretKey(
+    bs58.decode("4tuQjd1M2c5pB3P3u8qLyadm6ZPAVGtjcAbEya1ucoGfXtQoEf2JaKCAtLvFkJYdBUixXRg8HFywDAJtnT9MkQpq")
+  );
 
   const instruction = new TransactionInstruction({
-    keys: [{pubkey: tokenprogram, isSigner: false, isWritable: false},{pubkey: ata, isSigner: false, isWritable: true},{pubkey: mint, isSigner: false, isWritable: true},{pubkey: payer.publicKey, isSigner: true, isWritable: true},{pubkey: tokenmetaPubkeya, isSigner: false, isWritable: true}, {pubkey: tokenmetaPubkeyb, isSigner: false, isWritable: true}],
+    keys: [{pubkey: TOKEN_PROGRAM_ID, isSigner: false, isWritable: false},{pubkey: associatedtokenaccount, isSigner: false, isWritable: true},{pubkey: mint, isSigner: false, isWritable: true},{pubkey: payer.publicKey, isSigner: false, isWritable: true},{pubkey: tokenmetaPubkeya, isSigner: false, isWritable: true}, {pubkey: tokenmetaPubkeyb, isSigner: false, isWritable: true}],
     programId,
     data: Buffer.alloc(0),
   });
+
   // console.log('payer.pubkey', payer.publicKey.toBase58());
   // console.log('Instruction made >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>', instruction);
+
   const result = await sendAndConfirmTransaction(
     connection,
     new Transaction().add(instruction),
-    [payer],
+    [signer],
   );
-  // console.log(result);
+  console.log(result);
 }
--- src/program-rust/src/lib.rs
+++ src/program-rust/src/lib.rs
@@ -3,11 +3,11 @@ use solana_program::{
     account_info::{next_account_info, AccountInfo},
     entrypoint,
     entrypoint::ProgramResult,
-    program::invoke,
+    // program::invoke,
     msg,
     pubkey::Pubkey,
 };
-use spl_token::instruction::burn;
+// use spl_token::instruction::burn;
 
 #[derive(BorshSerialize, BorshDeserialize, Debug)]
 pub struct UniverseMetadata {
@@ -28,41 +28,48 @@ pub fn process_instruction(
     accounts: &[AccountInfo],
     _instruction_data: &[u8],
 ) -> ProgramResult {
+
     msg!("Hello World Rust program entrypoint");
 
     let accounts_iter = &mut accounts.iter();
 
-    let token_program = next_account_info(accounts_iter)?;
-    let burn_account = next_account_info(accounts_iter)?;
-    let mint = next_account_info(accounts_iter)?;
-    let authority = next_account_info(accounts_iter)?;
+    let _token_program = next_account_info(accounts_iter)?;
+    let _burn_account = next_account_info(accounts_iter)?;
+    let _mint = next_account_info(accounts_iter)?;
+    let _authority = next_account_info(accounts_iter)?;
     let metadata_account_info_a = next_account_info(accounts_iter)?;
-    let metadata_account_info_b = next_account_info(accounts_iter)?;
+    let _metadata_account_info_b = next_account_info(accounts_iter)?;
     
-    let mut metadata_account_a = UniverseMetadata::try_from_slice(&metadata_account_info_a.data.borrow())?;
-    let metadata_account_b = UniverseMetadata::try_from_slice(&metadata_account_info_b.data.borrow())?;
+    let metadata_account_a = UniverseMetadata::try_from_slice(&metadata_account_info_a.data.borrow())?;
+    // let mut metadata_account_a = UniverseMetadata::try_from_slice(&metadata_account_info_a.data.borrow())?;
+    // let metadata_account_b = UniverseMetadata::try_from_slice(&metadata_account_info_b.data.borrow())?;
 
-    metadata_account_a.current_universe_level = metadata_account_a.current_universe_level + metadata_account_b.current_universe_level;
+    // metadata_account_a.current_universe_level = metadata_account_a.current_universe_level + metadata_account_b.current_universe_level;
+    msg!("metadata_account_a.current_universe_level {}", metadata_account_a.current_universe_level);
 
-    let amount = 1;
+    // metadata_account_a.serialize(&mut *metadata_account_info_a.try_borrow_mut_data()?)?;
+    metadata_account_a.serialize(&mut &mut metadata_account_info_a.data.borrow_mut()[..])?;
 
-    let ix = burn(
-        token_program.key,
-        burn_account.key,
-        mint.key,
-        authority.key,
-        &[],
-        amount,
-    )?;
-    invoke(
-        &ix,
-        &[
-            burn_account.clone(),
-            mint.clone(),
-            authority.clone(),
-            token_program.clone(),
-        ],
-    )?;
+
+    let _amount = 1;
+
+    // let ix = burn(
+    //     token_program.key,
+    //     burn_account.key,
+    //     mint.key,
+    //     authority.key,
+    //     &[],
+    //     amount,
+    // )?;
+    // invoke(
+    //     &ix,
+    //     &[
+    //         burn_account.clone(),
+    //         mint.clone(),
+    //         authority.clone(),
+    //         token_program.clone(),
+    //     ],
+    // )?;
 
     Ok(())
 }
