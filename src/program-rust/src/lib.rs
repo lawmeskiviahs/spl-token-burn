233481e063424970c4a1fcde0fa38a3b54883dab
lawmeskiviahs <shaiviksemwal@gmail.com>
2022-05-10 12:12:53 +0530

    optimisation

--- src/program-rust/src/lib.rs
+++ src/program-rust/src/lib.rs
@@ -1,35 +1,23 @@
-use borsh::{BorshDeserialize, BorshSerialize};
 use solana_program::{
     account_info::{next_account_info, AccountInfo},
     entrypoint,
     entrypoint::ProgramResult,
     program::invoke,
     msg,
-    // program_error::ProgramError,
     pubkey::Pubkey,
 };
-// pub mod spl_token;
 use spl_token::instruction::burn;
 
-/// Define the type of state stored in accounts
-#[derive(BorshSerialize, BorshDeserialize, Debug)]
-pub struct GreetingAccount {
-    /// number of greetings
-    pub counter: u32,
-}
-
-// Declare and export the program's entrypoint
 entrypoint!(process_instruction);
 
-// Program entrypoint's implementation
 pub fn process_instruction(
-    _program_id: &Pubkey, // Public key of the account the hello world program was loaded into
-    accounts: &[AccountInfo], // The account to say hello to
-    _instruction_data: &[u8], // Ignored, all helloworld instructions are hellos
+    _program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    _instruction_data: &[u8]
 ) -> ProgramResult {
-    msg!("Hello World Rust program entrypoint");
+    
+    msg!("Watch your tokens get burnt");
 
-    // Iterating accounts is safer than indexing
     let accounts_iter = &mut accounts.iter();
 
     let token_program = next_account_info(accounts_iter)?;
